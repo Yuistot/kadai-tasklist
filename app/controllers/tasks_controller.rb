a433bc5e0350e01f9e66529d66c04c4bf292a78a
tsuyoshi.yanai <00823n@gmail.com>
2018-03-03 15:16:10 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'タスクが正常に追加されました'
+      redirect_to @task
+    else
+      flash.now[:danger] ='Tasnkの追加に失敗しました'
+      render :now
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'タスクが正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] ='タスクの更新に失敗しました'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'タスクは正常に破棄されました'
+    redirect_to tasks_url
+  end
+  
+  private
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
