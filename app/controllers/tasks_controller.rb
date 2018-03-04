c382f53237074bf50ea9548ec01142f42edd256d
tsuyoshi.yanai <00823n@gmail.com>
2018-03-04 07:59:42 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -19,16 +19,14 @@ class TasksController < ApplicationController
       redirect_to @task
     else
       flash.now[:danger] ='Tasnkの追加に失敗しました'
-      render :now
+      render :new
     end
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'タスクが正常に更新されました'
@@ -40,7 +38,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'タスクは正常に破棄されました'
@@ -48,6 +45,11 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
     params.require(:task).permit(:content, :status)
   end
